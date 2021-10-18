f2dbfdc87236718d624876451aac57345eff0c6e
nico-bellante <nicobellante+github@gmail.com>
2021-10-18 16:12:36 -0700

    omzh

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/discord/.zprofile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 brew install neovim
+brew install autojump
 brew install bat
 
 mkdir -p ~/.local/share/nvim/site/autoload/
