12f6038c7ad9a5ff94e06b1277206a2104337ed3
nico-bellante <nicobellante+github@gmail.com>
2021-10-18 14:33:29 -0700

    trying to get brew working

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/discord/.zprofile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+
 brew install neovim
 
 mkdir -p "~/.local/share/nvim/site/autoload/"
