fa21f1553288bb234403ffbf38101bcec9ac5fc3
nico-bellante <nicobellante+github@gmail.com>
2021-10-21 14:19:36 -0700

    adding zsh config

--- install.sh
+++ install.sh
@@ -5,14 +5,16 @@
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/discord/.zprofile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
+# zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ln -s -f ~/dotfiles/zsh/.aliases ~/.aliases
+ln -s -f ~/dotfiles/zsh/.zshrc ~/.zshrc
 
 brew install neovim
 brew install autojump
 brew install bat
 
 mkdir -p ~/.local/share/nvim/site/autoload/
-
 mkdir -p ~/.config/nvim
 
 ln -s -f ~/dotfiles/vim/* ~/.config/nvim
